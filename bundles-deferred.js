acbaf4e6c8a28b613cd5c89bba4629d169f3c22f
Tabari <shotbygkstr@gmail.com>
2026-01-04 00:28:48 -0500

    Add missing bundles-deferred.js file

---
+++ bundles-deferred.js
@@ -0,0 +1,6 @@
+// Deferred bundles code - loaded on demand for recommendations panel
+// This file is intentionally minimal to reduce main bundle size
+
+window.renderRecsPanel = function() {
+  console.log('[Recommendations] Panel render requested but not implemented yet');
+};
